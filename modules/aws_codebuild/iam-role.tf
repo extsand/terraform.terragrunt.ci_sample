394a769dbc2eecfb77a3642bdea308a0402f5134
Alexander <extsand@gmail.com>
2021-11-19 19:45:30 +0200

    iam modify

--- modules/aws_codebuild/iam-role.tf
+++ modules/aws_codebuild/iam-role.tf
@@ -2,35 +2,44 @@ resource "aws_iam_role" "codebuild-role" {
 	name = "role-for-codebuild"
 	assume_role_policy = data.template_file.template-codebuild-role.rendered	
 }
-resource "aws_iam_role_policy_attachment" "aws_codebuild_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+
+resource "aws_iam_role_policy" "codebuild-role-attachment" {
+	name = "policy-for-codebuild"
+	role = aws_iam_role.codebuild-role.name
+	policy = data.template_file.template-for-codebuild-role-policy.rendered
+	
 }
 
-resource "aws_iam_role_policy_attachment" "aws_cloudwatch_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "aws_iam_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "aws_key_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser"
-}
-resource "aws_iam_role_policy_attachment" "aws_s3_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-resource "aws_iam_role_policy_attachment" "aws_ec2_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-resource "aws_iam_role_policy_attachment" "aws_vpc_admin"{
-    role = aws_iam_role.codebuild-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-}
+# Add IAM Full access for Services
+# resource "aws_iam_role_policy_attachment" "aws_codebuild_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+# }
+
+# resource "aws_iam_role_policy_attachment" "aws_cloudwatch_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "aws_iam_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "aws_key_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser"
+# }
+# resource "aws_iam_role_policy_attachment" "aws_s3_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "aws_ec2_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "aws_vpc_admin"{
+#     role = aws_iam_role.codebuild-role.name
+#     policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+# }
 
 
 
