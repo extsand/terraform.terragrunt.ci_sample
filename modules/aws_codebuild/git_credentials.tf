b62ab20ec7a7c7af2a87ecaa7051bce1113051e9
Alexander <extsand@gmail.com>
2021-11-19 23:09:20 +0200

    modify git webhook

---
+++ modules/aws_codebuild/git_credentials.tf
@@ -0,0 +1,21 @@
+# # ======== Manual version
+# resource "null_resource" "import_credentials" {
+# 	triggers = {
+# 		github_oauth_token = var.git_token
+# 	}
+# 	provisioner "local-exec" {
+# 		command = <<EOF
+# 		 aws --region eu-central-1 codebuild import-source-credentials \
+#                                                              --token ${var.git_token} \
+#                                                              --server-type GITHUB \
+#                                                              --auth-type PERSONAL_ACCESS_TOKEN
+# EOF
+# 	}
+# }
+
+# #add credentials for github
+resource "aws_codebuild_source_credential" "github_token" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = var.git_token
+}
